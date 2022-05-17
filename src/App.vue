a8c1903a42635654e920949b4d3f11bd023a6795
k.yoshida <>
2022-05-17 13:59:58 +0900

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld @parent="parent" msg="propsで書き換えました。" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    parent: function (e) {
+      console.log(e);
+    },
+  },
+};
 </script>
 
 <style>
