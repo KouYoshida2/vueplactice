b1c9e0a08757be56827cfea5ecc8ab73ed10aae4
k.yoshida <>
2022-05-18 17:34:08 +0900

    0518kadai

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,65 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld @parent="parent" msg="propsで書き換えました。" />
+  <div class="cards">
+    <div v-for="(item, index) in items" :key="index">
+      <CardComponent
+        :id="item.id"
+        :image="item.image"
+        :title="item.title"
+        :description="item.description"
+      />
+    </div>
+  </div>
+  <HelloWorld @parent="parent" msg="propsで書き換えました。"></HelloWorld>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld";
+import HelloWorld from "@/components/HelloWorld.vue";
+import CardComponent from "@/components/CardComponent";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    CardComponent,
   },
   methods: {
     parent: function (e) {
       console.log(e);
     },
   },
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "タイトル1",
+          image: "img1.jpg",
+          description: "詳細1",
+        },
+        {
+          id: 2,
+          title: "タイトル2",
+          image: "img2.jpg",
+          description: "詳細2",
+        },
+        {
+          id: 3,
+          title: "タイトル3",
+          image: "img3.jpg",
+          description: "詳細3",
+        },
+      ],
+    };
+  },
 };
 </script>
 
 <style>
+.cards {
+  display: flex;
+  justify-content: space-around;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    id:{{ id }}<br />
+    title:{{ title }}<br />
+    iamge:{{ image }}<br />
+    description:{{ description }}
+  </div>
+</template>
+
+<script>
+export default {
+  // name: "CardComponent",
+  props: {
+    id: Number,
+    title: String,
+    image: String,
+    description: String,
+  },
+};
+</script>
+
+<style></style>
